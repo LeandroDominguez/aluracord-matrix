387cc22587d720a01e269d7271d3bd90702e4f84
Leandro Dominguez <leandrodmz@hotmail.com>
2022-01-28 10:28:00 -0300

    Segundo Commit

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,10 @@
 
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from 'react';
+import {useRouter} from 'next/router';
 import appConfig from '../config.json';
 
-function GlobalStyle() {
-    return (
-      <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-  }
+
   
 
 
@@ -72,16 +47,18 @@ function Titulo(props){
 //export default HomePage
 
 export default function PaginaInicial() {
-    const username = 'peas';
+    //const username = 'LeandroDominguez';
+    const [username, setUsername] = React.useState('LeandroDominguez');
+    const roteamento = useRouter();
+    
   
     return (
       <>
-        <GlobalStyle />
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
             backgroundColor: appConfig.theme.colors.primary[500],
-            backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
+            backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/07/simpsons-couch-1536x864.jpg)',
             backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
           }}
         >
@@ -103,6 +80,11 @@ export default function PaginaInicial() {
             {/* Formulário */}
             <Box
               as="form"
+              onSubmit={function(infosDoEvento){
+                infosDoEvento.preventDefault();
+                console.log('Alguem submeteu o form');
+                roteamento.push('/chat');
+              }}
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -112,8 +94,28 @@ export default function PaginaInicial() {
               <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
                 {appConfig.name}
               </Text>
-  
+
+
+              {/*<input 
+              type="text"
+              value={username}
+              onChange={function (event) {
+                console.log('Usuário Digitou', event.target.value)
+                //onde está o valor?
+                const valor = event.target.value;
+                //Trocar o valor da variável através do react pela função setusername
+                setUsername(valor);
+              }}
+               />*/}
               <TextField
+                value={username}
+                onChange={function (event) {
+                  console.log('Usuário Digitou', event.target.value)
+                  //onde está o valor?
+                  const valor = event.target.value;
+                  //Trocar o valor da variável através do react pela função setusername
+                  setUsername(valor);
+                }}
                 fullWidth
                 textFieldColors={{
                   neutral: {
